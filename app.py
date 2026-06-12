06f199cc0ea3b43b69b2f625b45d1b3149855395
iam-manoj <manojtmk2323@gmail.com>
2026-06-12 18:29:36 +0000

    this is for blue/green

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"congratulations": "your flask app is running in a container! with automation from github actions and deployed to AWS ECS!  "}
+    return {"congratulations": "your flask app is running in a container! with blue/green deployment strategy  "}
 
 @app.route("/health")
 def health():
